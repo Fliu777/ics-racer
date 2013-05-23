b03daf763798aad7bff88b58e17494ecf402ee98
m33kcow@gmail.com <m33kcow@gmail.com@a9e20f3d-4695-1544-b0e7-8aa0c13e49cd>
2013-05-23 15:18:30 +0000

    Mini-Update GameMap - Michael

--- src/GameMap.java
+++ src/GameMap.java
@@ -1,11 +1,13 @@
+import java.awt.*;
 import java.io.*;
+import java.util.*;
 
 public class GameMap {
 	
 	private File fileMapRaw;
 	private FileReader fileMapStream;
 	private BufferedReader mapData;
-	private char[] mapArray;
+	private ArrayList<String> textMap;
 	
 	public GameMap() {
 		fileMapRaw = new File("TextMap.txt");
@@ -14,6 +16,22 @@ public class GameMap {
 			mapData = new BufferedReader(fileMapStream);
 		} catch (FileNotFoundException e) {
 			System.err.println("FileNotFoundException: " + e.getMessage());
+		}
+		textMap = new ArrayList<String>();
+		try {
+			String line;
+			while (mapData.readLine() != null) {
+				line = mapData.readLine();
+				for (int i = 0; i<line.length(); i++) textMap.add(line.substring(i,i+1));
+			}
+		} catch (IOException e) {
+			System.err.println("IOException: " + e.getMessage());
+		}
+	}
+	
+	public void draw(Graphics g) {
+		for (int i = 0; i<textMap.size(); i++) {
+			
 		}
 	}
 }
