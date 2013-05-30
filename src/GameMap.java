9533b77164701e17d1ac6aee3f5538c0bdd4db6a
m33kcow@gmail.com <m33kcow@gmail.com@a9e20f3d-4695-1544-b0e7-8aa0c13e49cd>
2013-05-30 14:06:30 +0000

    dammit micahel

--- src/GameMap.java
+++ src/GameMap.java
@@ -14,6 +14,8 @@ public class GameMap {
 	private int tempHeight;
 	private BufferedImage mapNS, mapEW, mapNE, mapSE, mapSW, mapNW, mapOR, map4Way;
 	private BufferedImage[] roadArray;
+	private boolean up,down,left,right;
+	private String[][] textArray;
 	
 	public GameMap() {
 		
@@ -38,7 +40,7 @@ public class GameMap {
 		
 		//Adding new Levels
 		fileMapArray = new ArrayList<File>();
-		fileMapArray.add(new File("src/Levels/TextMap")); 
+		fileMapArray.add(new File("src/Levels/TextMap"));
 		
 		try {
 			fileMapStream = new FileReader(fileMapArray.get(0));
@@ -55,6 +57,12 @@ public class GameMap {
 		} catch (IOException e) {
 			System.err.println("IOException: " + e.getMessage());
 		}
+		textArray = new String[textMap.get(0).length()][textMap.size()];
+		for (int i = 0; i<textArray.length; i++) {
+			for (int j = 0; j<textArray[0].length; j++) {
+				textArray[i][j] = textMap.get(j).substring(i,i+1);
+			}
+		}
 		width = MainLoop.ScreenWidth;
 		height = MainLoop.ScreenHeight-30;
 		tempHeight = textMap.size();
@@ -64,25 +72,54 @@ public class GameMap {
 		String[][] newMap = new String[7][7];
 		for (int i = 0; i<7; i++) {
 			for (int j = 0; j<7; j++) {
-				newMap[i][j] = textMap.get(x+i).substring(y+j,y+j+1);
+				newMap[i][j] = textArray[x+i][y+j];
 			}
 		}
 		return newMap;
 	}
 	
-	
+	public void mapConvert(int x, int y) {
+		up=down=left=right=false;
+		if (!(textArray[x+1][y].equals("."))) down=true;
+		if (!(textArray[x-1][y].equals("."))) up=true;
+		if (!(textArray[x][y+1].equals("."))) right=true;
+		if (!(textArray[x][y-1].equals("."))) left=true;
+		//base case return if all 4 sides do not have modifiable paths
+		if (!(textArray[x-1][y].equals("X"))&&!(textArray[x+1][y].equals("X"))&&!(textArray[x][y-1].equals("X"))&&!(textArray[x][y+1].equals("X"))) return;
+		if (up&&down) textArray[x][y] = "|";
+		else if (left&right) textArray[x][y] = "-";
+		else if (up&&right) textArray[x][y] = "1";
+		else if (down&&right) textArray[x][y] = "2";
+		else if (down&&left) textArray[x][y] = "3";
+		else if (up&&left) textArray[x][y] = "4";
+		if (textArray[x-1][y].equals("X")) mapConvert(x-1,y);
+		if (textArray[x+1][y].equals("X")) mapConvert(x+1,y);
+		if (textArray[x][y-1].equals("X")) mapConvert(x,y-1);
+		if (textArray[x][y+1].equals("X")) mapConvert(x,y+1);
+	}
 	
 	public void draw(Graphics g) {
+		mapConvert(2,3);
+		for (int i = 0; i<textArray.length; i++) {
+			for (int j = 0; j<textArray[0].length; j++) {
+				System.out.print(textArray[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("\n\n\n\n\n\n\nQOOOOOo");
 		String[][] map = currentMap(0,0);
 		for (int i = 0; i<7; i++) {
 			for (int j = 0; j<7; j++) {
+				System.out.print(map[i][j]);
 				if (map[i][j].equals(".")) {
 					g.drawImage(roadArray[7],i*200, j*200, null);
 				}
-				else {
-					g.drawImage(roadArray[3], i*200, j*200, null);
+				else if (map[i][j].equals("-")){
+					//g.drawImage(roadArray[0], i*200, j*200, null);
 				}
 			}
+			System.out.println();
 		}
+		System.out.println("SDSADS\n\n\n\n\n\n");
 	}
 }
