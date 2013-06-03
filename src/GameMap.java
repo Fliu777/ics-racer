37f5df5e31f6b3d67c6d881510bac2254cbaa295
m33kcow@gmail.com <m33kcow@gmail.com@a9e20f3d-4695-1544-b0e7-8aa0c13e49cd>
2013-06-03 00:37:54 +0000

    Map scrolling test independent of player movement

--- src/GameMap.java
+++ src/GameMap.java
@@ -20,7 +20,10 @@ public class GameMap {
 	private BufferedImage mapNS, mapEW, mapNE, mapSE, mapSW, mapNW, mapOR, map4Way; //Image block variables
 	private BufferedImage[] roadArray;
 	private boolean up,down,left,right; //Used for block detection in recursive pathing
-	private String[][] textArray;
+	private String[][] textArray, map; //Converted map arrays
+	private Graphics g;
+	private double vx, vy; //Map scrolling
+	private int mapX, mapY;
 	
 	public GameMap() {
 		
@@ -72,9 +75,15 @@ public class GameMap {
 		for (int i = 0; i<textArray.length; i++) {
 			for (int j = 0; j<textArray[0].length; j++) textArray[i][j] = textMap.get(i).substring(j,j+1);
 		}
+		
+		//Map scrolling
+		mapX = mapY = 0;
+		vx = vy = 0.0;
+		map = currentMap(mapX,mapY);
 	}
 	
-	public String[][] currentMap(int x, int y) {
+	//Clipping a portion of the map
+	 public String[][] currentMap(int x, int y) {
 		String[][] newMap = new String[10][10];
 		for (int i = 0; i<10; i++) {
 			for (int j = 0; j<10; j++) {
@@ -83,7 +92,7 @@ public class GameMap {
 		}
 		return newMap;
 	}
-	
+	 
 	//Recursive path finding method and map conversion for easier reading
 	public void mapConvert(int x, int y) {
 		up=down=left=right=false; //Sets all surrounding blocks as false (no path)
@@ -113,23 +122,47 @@ public class GameMap {
 		if (textArray[x][y+1].equals("X")) mapConvert(x,y+1);
 	}
 	
-	public void draw(Graphics g) {
-		mapConvert(1,1);
-		String[][] map = currentMap(0,0);
-		for (int i = 0; i<10; i++) {
-			for (int j = 0; j<10; j++) {
-				//System.out.print(map[i][j]);
-				if (map[i][j].equals("|")) g.drawImage(roadArray[0], j*200, i*200, null);
-				else if (map[i][j].equals("-")) g.drawImage(roadArray[1], j*200, i*200, null);
-				else if (map[i][j].equals("1")) g.drawImage(roadArray[2], j*200, i*200, null);
-				else if (map[i][j].equals("2")) g.drawImage(roadArray[3], j*200, i*200, null);
-				else if (map[i][j].equals("3")) g.drawImage(roadArray[4], j*200, i*200, null);
-				else if (map[i][j].equals("4")) g.drawImage(roadArray[5], j*200, i*200, null);
-				else if (map[i][j].equals("+")) g.drawImage(roadArray[6], j*200, i*200, null);
-				else if (map[i][j].equals(".")) g.drawImage(roadArray[7],j*200, i*200, null);
+	//Finds the first piece of road
+	public int findX(int coord) {
+		for (int i = 0; i<textArray.length; i++) {
+			for (int j = 0; j<textArray[0].length; j++) {
+				if (textArray[i][j].equals("X")&&coord==0) return i;
+				else if (textArray[i][j].equals("X")&&coord==1) return j;
 			}
-			//System.out.println();
 		}
-		//System.out.println("\n\n\n\n");
+		return 0;
+	}
+	
+	//Drawing the current map
+	public void drawCurrentMap(String[][] map, double vx, double vy) {
+		for (int i = 0; i<map.length; i++) {
+			for (int j = 0; j<map[0].length; j++) {
+				if (map[i][j].equals("|")) g.drawImage(roadArray[0], (int)Math.round(j*200-vx), (int)Math.round(i*200-vy), null);
+				else if (map[i][j].equals("-")) g.drawImage(roadArray[1], (int)Math.round(j*200-vx), (int)Math.round(i*200-vy), null);
+				else if (map[i][j].equals("1")) g.drawImage(roadArray[2], (int)Math.round(j*200-vx), (int)Math.round(i*200-vy), null);
+				else if (map[i][j].equals("2")) g.drawImage(roadArray[3], (int)Math.round(j*200-vx), (int)Math.round(i*200-vy), null);
+				else if (map[i][j].equals("3")) g.drawImage(roadArray[4], (int)Math.round(j*200-vx), (int)Math.round(i*200-vy), null);
+				else if (map[i][j].equals("4")) g.drawImage(roadArray[5], (int)Math.round(j*200-vx), (int)Math.round(i*200-vy), null);
+				else if (map[i][j].equals("+")) g.drawImage(roadArray[6], (int)Math.round(j*200-vx), (int)Math.round(i*200-vy), null);
+				else if (map[i][j].equals(".")) g.drawImage(roadArray[7], (int)Math.round(j*200-vx), (int)Math.round(i*200-vy), null);
+			}
+		}
+	}
+	
+	public void draw(Graphics g) {
+		this.g = g;
+		vx++;
+		vy++;
+		if (vx>200) {
+			vx=0;
+			mapX++;
+		}
+		if (vy>200) {
+			vy=0;
+			mapY++;
+		}
+		mapConvert(findX(0),findX(1));
+		String[][] map = currentMap(mapX,mapY);
+		drawCurrentMap(map, vx, vy);
 	}
 }
